3f469faff763f367f26a371f1d057df0543bc0aa
andreafavia <andrea.favia@sleepiz.com>
2022-07-25 17:15:18 +0200

    Add Converter class

--- yaket/trainer.py
+++ yaket/trainer.py
@@ -6,6 +6,7 @@ import numpy as np
 import tensorflow as tf
 import gc
 from yaket.schema.schema import TrainingModel, yaml_to_pydantic, Accelerator
+from yaket.converter.converter import Converter
 import importlib
 import mlflow
 import os
@@ -25,7 +26,6 @@ class Trainer:
     validate_yaml: bool = True
     custom_modules_path: Optional[str] = None
 
-
     # internals
     _config: TrainingModel = None
     _input_shape: Tuple[int, ...] = None
@@ -37,24 +37,30 @@ class Trainer:
     _history: Dict[str, Any] = None
     _model_checkpoint: Optional[str] = None
     _accelerator: Optional[Accelerator] = None
-    _log:bool = False
-    
+    _log: bool = False
+    _out_path: str = None
 
     def _init_trainer(self) -> None:
         """Initialize the trainer"""
 
         if not isinstance(self.model, tf.keras.models.Model):
-            raise Exception('model must be keras model')
-        if not isinstance(self.config_path,str) or not os.path.isfile(self.config_path):
-            raise Exception('Config path must be a valid file path')
-        if self.strategy is not None and not isinstance(self.strategy, tf.distribute.Strategy):
+            raise Exception("model must be keras model")
+        if not isinstance(self.config_path, str) or not os.path.isfile(
+            self.config_path
+        ):
+            raise Exception("Config path must be a valid file path")
+        if self.strategy is not None and not isinstance(
+            self.strategy, tf.distribute.Strategy
+        ):
             raise Exception("Strategy must be keras strategy object")
         if not isinstance(self.random_seed, int):
             raise Exception("Random seed must be an integer")
         if not isinstance(self.validate_yaml, bool):
             raise Exception("Validate yaml must be a boolean value")
         if self.custom_modules_path is not None:
-            if not isinstance(self.custom_modules_path, str) or not os.path.isfile(self.custom_modules_path):
+            if not isinstance(self.custom_modules_path, str) or not os.path.isfile(
+                self.custom_modules_path
+            ):
                 raise Exception("Costum modules path must be a valid path string")
 
         if self.custom_modules_path:
@@ -66,29 +72,28 @@ class Trainer:
         self._callbacks = self._get_callbacks()
         self._input_shape = self._get_input_shape()
 
-    
-
     def train(self, epochs: int = None):
         """Train the model. Main function to call.
-        
+
         Args
         ----
         epochs: int, optional
             Number of epochs to train. If None, will train with the number of epochs specified in the config file.
-        
+
         """
 
         self._init_trainer()
         self._autolog()
 
         strategy = self._get_strategy()
-        batch_size = strategy.num_replicas_in_sync*self.config.batch_size
-        train_dataset = self._get_x_y_train(batch_size) 
+        batch_size = strategy.num_replicas_in_sync * self.config.batch_size
+        train_dataset = self._get_x_y_train(batch_size)
         val_dataset = self._get_x_y_val(batch_size)
-        
+
         with strategy.scope():
 
-            if self.strategy is None: self._clone_model() 
+            if self.strategy is None:
+                self._clone_model()
             self._compile_model()
 
             history = self.model.fit(
@@ -98,15 +103,52 @@ class Trainer:
                 validation_data=val_dataset,
                 batch_size=None,
                 callbacks=self._callbacks,
-                class_weight=None, #TODO: add class_weight,
+                class_weight=None,  # TODO: add class_weight,
                 verbose=int(self.config.verbose),
             )
-
 
         self._save_model()
         self._history = history.history
         self._clean_workspace()
         return history
+
+    def convert_model(
+        self,
+        format_model: str = "onnx",
+        opset_onnx: int = 15,
+        output_path: str = "model",
+        from_command_line: bool = True,
+    ):
+
+        """Convert the model to a different format. Available formats:
+        1. ONNX
+        2. TensorFlow Lite
+
+        Args
+        ----
+        format: str
+            Format to convert the model to. Available formats: onnx, tflite
+        model_path: str
+            Path to the model to convert.
+        output_path: str
+            Path to the output file.
+        from_command_line: bool
+            Whether or not to convert the model using the command line.
+            It might be not available with some platforms.
+
+        """
+        if from_command_line and self._out_path is None:
+            raise Exception("You need to have a saved model before converting using command line")
+        model_to_convert = self.model if not from_command_line else None
+        converter = Converter(
+            out_format=format_model.lower(),
+            opset_onnx=opset_onnx,
+            model_path=self._out_path,
+            model=model_to_convert,
+            out_path = output_path,
+        )
+        if converter.convert():
+            print(f"Successfully converted to format {format} ")
 
     def _get_input_shape(self):
         """Get the input shape of input dataset"""
@@ -122,46 +164,55 @@ class Trainer:
         if self._model_checkpoint is not None:
             self.model.load_weights(self._model_checkpoint)
             if self._log:
-                self.model.save('/tmp/best_model')
+                self.model.save("/tmp/best_model")
                 run = mlflow.last_active_run()
-                idx = 7 #TODO: check is always the same
+                idx = 7  # TODO: check is always the same
                 artifact_path = run.info.artifact_uri[idx:]
-                self.model.save(artifact_path+f"/best_model")
+                self._out_path = artifact_path + f"/best_model"
+                self.model.save(self._out_path)
             else:
-                os.makedirs(os.getcwd()+'/models', exist_ok=True)
+                os.makedirs(os.getcwd() + "/models", exist_ok=True)
                 t = int(time.time())
-                self.model.save(os.getcwd()+f"/models/{t}_best_model")
+                self._out_path = os.getcwd() + f"/models/{t}_best_model"
+                self.model.save(self._out_path)
         else:
-            os.makedirs(os.getcwd()+'/models', exist_ok=True)
+            os.makedirs(os.getcwd() + "/models", exist_ok=True)
             t = int(time.time())
-            self.model.save(os.getcwd()+f"/models/{t}_best_model")
-    
+            self._out_path = os.getcwd() + f"/models/{t}_best_model"
+            self.model.save(self._out_path)
+
     def _clone_model(self):
         """Clone the model so that it works within tf.distribute.Strategy
-            It works only for models not using custom objects
+        It works only for models not using custom objects
         """
         self.model = tf.keras.models.clone_model(self.model)
-
 
     def _get_x_y_val(self, batch_size):
         """Get the x and y for training based on the format of the dataset"""
         options = tf.data.Options()
-        options.experimental_distribute.auto_shard_policy = tf.data.experimental.AutoShardPolicy.OFF
+        options.experimental_distribute.auto_shard_policy = (
+            tf.data.experimental.AutoShardPolicy.OFF
+        )
 
         if self.val_dataset is None:
             return None
         if isinstance(self.val_dataset, tf.data.Dataset):
             return self.val_dataset
         else:
-            val = tf.data.Dataset.from_tensor_slices(self.val_dataset)\
-                .batch(batch_size).with_options(options)
+            val = (
+                tf.data.Dataset.from_tensor_slices(self.val_dataset)
+                .batch(batch_size)
+                .with_options(options)
+            )
             return val
 
     def _get_x_y_train(self, batch_size):
         """Get the x and y for training based on the format of the dataset"""
         options = tf.data.Options()
-        options.experimental_distribute.auto_shard_policy = tf.data.experimental.AutoShardPolicy.OFF
-    
+        options.experimental_distribute.auto_shard_policy = (
+            tf.data.experimental.AutoShardPolicy.OFF
+        )
+
         if isinstance(self.train_dataset, tf.data.Dataset):
             x = self.train_dataset
         else:
@@ -181,27 +232,30 @@ class Trainer:
         self._optimizer = self._get_optimizer()
         self._loss = self._get_loss()
         self._metrics = self._get_metrics()
-        
+
         self.model.compile(
             optimizer=self._optimizer, loss=self._loss, metrics=self._get_metrics()
         )
 
     def _get_strategy(self):
         if self.strategy is None:
-            if self._accelerator is None :
+            if self._accelerator is None:
                 return tf.distribute.MirroredStrategy()
             if self._accelerator is Accelerator.gpu:
                 index = Trainer.get_free_gpu_idx()
                 return tf.distribute.OneDeviceStrategy(f"/gpu:{index}")
-            if self._accelerator is Accelerator.cpu or self._accelerator is Accelerator.mgpu:
+            if (
+                self._accelerator is Accelerator.cpu
+                or self._accelerator is Accelerator.mgpu
+            ):
                 # If GPUs are not available, it will use CPUs
                 return tf.distribute.MirroredStrategy()
             if self._accelerator is Accelerator.tpu:
-                #TODO: check configuration for tpu strategy
+                # TODO: check configuration for tpu strategy
                 return tf.distribute.TPUStrategy()
         else:
             return self.strategy
-    
+
     @staticmethod
     def get_free_gpu_idx():
         """Get the index of the freer GPU"""
@@ -211,7 +265,6 @@ class Trainer:
         )
         memory_free_values = [int(x.split()[0]) for i, x in enumerate(memory_free_info)]
         return int(np.argmin(memory_free_values))
-
 
     def _parse_config(self) -> Any:
         return yaml_to_pydantic(self.config_path, self.validate_yaml)
@@ -231,7 +284,7 @@ class Trainer:
         try:
             custom_dirpath = os.path.dirname(module_name)
             sys.path.append(custom_dirpath)
-            module = module_name.split('/')[-1].split('.')[0]
+            module = module_name.split("/")[-1].split(".")[0]
             self._custom_module = importlib.import_module(module)
         except Exception as e:
             raise ImportError(f"Error importing {module_name}: {e}")
@@ -284,20 +337,30 @@ class Trainer:
                 args = list(name_callback.values())[0]
 
                 # Track filepath if it's a ModelCheckpoint
-                self._model_checkpoint =args['filepath'] if key == 'ModelCheckpoint' else None
+                self._model_checkpoint = (
+                    args["filepath"] if key == "ModelCheckpoint" else None
+                )
 
                 callback_value = getattr(tf.keras.callbacks, key, default_value)
             else:
-                callback_value = getattr(tf.keras.callbacks, name_callback, default_value)
+                callback_value = getattr(
+                    tf.keras.callbacks, name_callback, default_value
+                )
                 args = None
 
             if callback_value != default_value:
-                callbacks.append(callback_value(**args) if args is not None else callback_value())
+                callbacks.append(
+                    callback_value(**args) if args is not None else callback_value()
+                )
             else:
                 if args is not None:
-                    if args.get('dataset') is not None:
-                        dataset = self.val_dataset if args['dataset'] in ['val','dev','validation'] else self.train_dataset
-                        args['dataset'] = dataset
+                    if args.get("dataset") is not None:
+                        dataset = (
+                            self.val_dataset
+                            if args["dataset"] in ["val", "dev", "validation"]
+                            else self.train_dataset
+                        )
+                        args["dataset"] = dataset
                     callbacks.append(self._load_custom_module(key, args))
                 else:
                     callbacks.append(self._load_custom_module(name_callback, args))
@@ -314,13 +377,13 @@ class Trainer:
         for metric in self.config.metrics:
             if metric is None:
                 continue
-            if isinstance(metric,str):   
+            if isinstance(metric, str):
                 args = None
                 metric_value = getattr(tf.keras.metrics, f"{metric}", default_value)()
             else:
                 m, args = list(metric.items())[0]
                 metric_value = getattr(tf.keras.metrics, f"{m}", default_value)(**args)
-            
+
             if metric_value != default_value:
                 list_metrics.append(metric_value)
             else:
@@ -358,8 +421,7 @@ class Trainer:
             else:
                 tf.random.set_seed(random_seed)
                 np.random.seed(random_seed)
-                
-                   
+
     def clear_ram(self):
         "Delete model and all datasets saved in the Trainer class"
         del self.model
@@ -373,8 +435,7 @@ class Trainer:
         self.model.summary()
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     import numpy as np
     from tensorflow import keras
     from tensorflow.keras import layers
@@ -396,7 +457,6 @@ if __name__ == '__main__':
     print(x_train.shape[0], "train samples")
     print(x_test.shape[0], "test samples")
 
-
     # convert class vectors to binary class matrices
     y_train = keras.utils.to_categorical(y_train, num_classes)
     y_test = keras.utils.to_categorical(y_test, num_classes)
@@ -404,24 +464,27 @@ if __name__ == '__main__':
     strategy = tf.distribute.MirroredStrategy()
     with strategy.scope():
         model = keras.Sequential(
-        [
-            keras.Input(shape=input_shape),
-            layers.Conv2D(32, kernel_size=(3, 3), activation="relu"),
-            layers.MaxPooling2D(pool_size=(2, 2)),
-            layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
-            layers.MaxPooling2D(pool_size=(2, 2)),
-            layers.Flatten(),
-            layers.Dropout(0.5),
-            layers.Dense(num_classes, activation="softmax"),
-        ]
+            [
+                keras.Input(shape=input_shape),
+                layers.Conv2D(32, kernel_size=(3, 3), activation="relu"),
+                layers.MaxPooling2D(pool_size=(2, 2)),
+                layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
+                layers.MaxPooling2D(pool_size=(2, 2)),
+                layers.Flatten(),
+                layers.Dropout(0.5),
+                layers.Dense(num_classes, activation="softmax"),
+            ]
         )
 
     model.summary()
 
-    
+    path = "/root/project/yaket/examples/files/trainer.yaml"
 
-    path = '/root/project/yaket/examples/files/trainer.yaml'
-    
-    trainer = Trainer(config_path = path, train_dataset=(x_train, y_train), val_dataset=(x_test, y_test), \
-            model=model, strategy=strategy)
+    trainer = Trainer(
+        config_path=path,
+        train_dataset=(x_train, y_train),
+        val_dataset=(x_test, y_test),
+        model=model,
+        strategy=strategy,
+    )
     trainer.train()
